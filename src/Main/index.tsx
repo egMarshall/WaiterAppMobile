66b990ef6b45fb54f08649d718c203924a0fe842
Érico Marshall <ericogmarshall1@gmail.com>
2023-03-01 15:47:17 -0300

    modal de selecionar mesa concluído.

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+
 import { Container, CategoriesContainer, MenuContainer, Footer, FooterContainer } from './styles';
 
 import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
 import { Button } from '../components/Button';
+import { TableModal } from '../components/TableModal';
 
 export const Main = () => {
+  const [isTableModalVisible, setTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -24,11 +34,19 @@ export const Main = () => {
 
       <Footer>
         <FooterContainer>
-          <Button onPress={() => alert('Novo Pedido')}>
-            Novo Pedido
-          </Button>
+          {!selectedTable && (
+            <Button onPress={() => setTableModalVisible(true)}>
+                      Novo Pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
 
   );
