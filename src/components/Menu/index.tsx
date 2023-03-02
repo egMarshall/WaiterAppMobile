4bc2854f67712f4e8d4eae0256a36cd86c5cd61c
Érico Marshall <ericogmarshall1@gmail.com>
2023-03-02 09:40:33 -0300

    Modal de produtos concluído.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,49 +1,68 @@
+import { useState } from 'react';
 import { FlatList, TouchableOpacity } from 'react-native';
 
 import { products } from '../../mocks/products';
+import { Product } from '../../types/Product';
 import { formatCurrency } from '../../utils/formatCurrency';
 import { PlusCircle } from '../Icons/PlusCircle';
+import { ProductModal } from '../ProductModal';
 import { Text } from '../Text';
 
-import { Product, ProductImage, ProductDetails, Separator, AddToCartButton } from './styles';
+import { ProductContainer, ProductImage, ProductDetails, Separator, AddToCartButton } from './styles';
 
 export function Menu() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<null | Product>(null);
+
+  function handleOpenModal(product: Product) {
+    setIsModalVisible(true);
+    setSelectedProduct(product);
+  }
+
   return (
-    <FlatList
-      data={products}
-      style={{ marginTop: 32}}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      keyExtractor={product => product._id}
-      ItemSeparatorComponent={Separator}
-      renderItem={({ item: product} ) => (
-        <Product>
-          <ProductImage
-            source={{
-              uri: `http://localhost:3001/uploads/${product.imagePath}`,
-            }}
-          />
+    <>
+      <ProductModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+        product={selectedProduct}
+      />
+      <FlatList
+        data={products}
+        style={{ marginTop: 32}}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        keyExtractor={product => product._id}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item: product} ) => (
+          <ProductContainer onPress={() => handleOpenModal(product)}>
+            <ProductImage
+              source={{
+                uri: `http://localhost:3001/uploads/${product.imagePath}`,
+              }}
+            />
 
-          <ProductDetails>
-            <Text weight='600'>
-              {product.name}
-            </Text>
-            <Text size={14} color="#666" style={{marginVertical: 8}}>
-              {product.description}
-            </Text>
-            <Text size={14} weight='600'>
-              {formatCurrency(product.price)}
-            </Text>
-          </ProductDetails>
+            <ProductDetails>
+              <Text weight='600'>
+                {product.name}
+              </Text>
+              <Text size={14} color="#666" style={{marginVertical: 8}}>
+                {product.description}
+              </Text>
+              <Text size={14} weight='600'>
+                {formatCurrency(product.price)}
+              </Text>
+            </ProductDetails>
 
-          <AddToCartButton>
-            <TouchableOpacity>
-              <PlusCircle/>
-            </TouchableOpacity>
-          </AddToCartButton>
+            <AddToCartButton>
+              <TouchableOpacity>
+                <PlusCircle/>
+              </TouchableOpacity>
+            </AddToCartButton>
 
-        </Product>
-      )}
+          </ProductContainer>
+        )}
 
-    />
+      />
+    </>
+
   );
 }
